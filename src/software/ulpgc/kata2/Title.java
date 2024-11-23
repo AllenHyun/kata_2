6b9ded7ea7e1b4278f9a25fb2174a886f3441eb8
AllenHyun <126077150+AllenHyun@users.noreply.github.com>
2024-11-23 19:07:04 +0000

    Create Title.java
    we add the class Title with id, titleType and primaryTitle. We also add two enums, TitleType and Genres

---
+++ src/software/ulpgc/kata2/Title.java
@@ -0,0 +1,36 @@
+package software.ulpgc.kata2;
+
+public class Title {
+    private final String id;
+    private final TitleType titleType;
+    private final String primaryTitle;
+
+    public Title(String id, TitleType titleType, String primaryTitle) {
+        this.id = id;
+        this.titleType = titleType;
+        this.primaryTitle = primaryTitle;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public TitleType getTitleType() {
+        return titleType;
+    }
+
+    public String getPrimaryTitle() {
+        return primaryTitle;
+    }
+
+    public enum TitleType{
+        VideoGame, TvPilot, Movie, TvSeries, TvMiniSeries, Short, TvSpecial, TvShort, Video, TvMovie, TvEpisode
+    }
+
+    public enum Genre{
+        Action, Adult, Adventure, Animation, Biography, Comedy, Crime, Documentary, Drama, Family, Fantasy,
+        FilmNoir, GameShow, genres, History, Horror, Music, Musical, Mistery, News, RealityTv, Romance, SciFi, Short,
+        Sport, TalkShow, Thriller, War, Western
+    }
+
+}
